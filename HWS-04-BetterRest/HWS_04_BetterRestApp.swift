20a5589bb8acbe7c9f3933a2b8bdc82fd6ff165b
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-03-23 19:48:14 +0100

    Initial Commit

---
+++ HWS-04-BetterRest/HWS_04_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_04_BetterRestApp.swift
+//  HWS-04-BetterRest
+//
+//  Created by Manuel Casocavallo on 23/03/21.
+//
+
+import SwiftUI
+
+@main
+struct HWS_04_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
